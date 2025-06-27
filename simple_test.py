991e5ab055f189b8c25d1fda2e4cb05725035017
Suzuki natsuhiro <natsus@suzukinoMacBook-Air.local>
2025-06-27 16:27:26 +0900

    Revert "Remove duplicate app.py and requirements.txt from Desktop/bridge/Desktop/bridge/; unify to root"
    This reverts commit 699cf3aa9634c63ffaf68c91cf4a13382f79c762.

--- simple_test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Simple Test App")
-st.write("Hello, World!")
-
-if st.button("Click me"):
-    st.write("Button clicked!")
-
-st.write("This is a test to see if Streamlit is working.")
